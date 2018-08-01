341ecae66fa231385c3db63a2e7048ca0148bd09
Ivan Mironov <mironov.ivan@gmail.com>
2018-08-01 21:44:30 +0300

    Travis-CI: More fixes.

--- tools/scons/files/pywrap.sh
+++ tools/scons/files/pywrap.sh
@@ -5,11 +5,4 @@ case "${0##*/}" in
 	*) arg1="$0.py" ;;
 esac
 
-for bin in python2.7 python2.6 python2.5 python2.4; do
-    case "$($bin -V 2>&1)" in
-        "Python 2"*) exec $bin $arg1 "$@" ;;
-    esac
-done
-
-echo "Unable to find a Python 2.x interpreter for executing ${arg1:+$arg1 }$@ !" >&2
-exit 1
+exec python2.7 $arg1 "$@"
